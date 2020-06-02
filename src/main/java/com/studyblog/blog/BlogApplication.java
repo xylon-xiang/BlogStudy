cd3137508d008ad2e21224e09320e9b8985dde00
legion-xylon <legion-xylon@Legion-Xylon>
2020-06-02 23:05:43 +0800

    test

--- src/main/java/com/studyblog/blog/BlogApplication.java
+++ src/main/java/com/studyblog/blog/BlogApplication.java
@@ -1,13 +1,15 @@
-package com.studyblog.blog.demo;
+package com.studyblog.blog;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BlogApplication.class, args);
     }
+
 
 }
